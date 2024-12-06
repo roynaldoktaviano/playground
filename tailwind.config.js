ba012eae97e562f46e4685b08a08102a1d2c77f1
Roynald Oktaviano <76274014+roynaldoktaviano@users.noreply.github.com>
2024-12-07 00:11:50 +0700

    gif

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     },
     extend: {
       backgroundImage:{
-        'main': "url(./assets/bg.jpg)"
+        'main': "url(./assets/bg.jpg)",
+        'clr': "url(./assets/bgg.png)",
+        'time': "url(./assets/kad.png)",
+        'wkt': "url(./assets/wktu.png)"
+
       }
     },
   },
